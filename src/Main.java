702250ae824a2e0f167e291e23dad0b8da8bf35e
jprodriguez33 <johnpaul334@icloud.com>
2023-01-13 11:46:30 -0500

    BinarySearch Homework

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         int target = 565;
-        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+        int[] array = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
         int first = 0;
-        int last = 31;
+        int last = array.length-1;
         int mid = (first+last)/2;
 
-        if (target == array[mid]) {
-            System.out.print(mid);
-        }else if (target > array[mid]) {
+    while(first <= last) {
+        if (array[mid] < target) {
             first = mid + 1;
-        }else {
+        } else if (array[mid] == target) {
+            System.out.println(" The target number is "+ mid);
+            break;
+        } else {
             last = mid - 1;
         }
-        if (target > last) {
-            System.out.println("Target number not found!");
+        mid = (first + last) / 2;
+         }
+        if (first > last) {
+            System.out.println("Target number is out of bounds");
         }
 
     }
